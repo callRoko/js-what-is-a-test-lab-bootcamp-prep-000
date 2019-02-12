7229756ac320c21728a371f21395b923c4f56b1c
Hazel Donaldson <hazel90.hd@gmail.com>
2019-02-12 16:16:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 35
-var message = ` is ${height} less than 40 inches tall`
+var message = `${name} is ${height} less than 40 inches tall`
 
 module.exports = {name, height, message}
